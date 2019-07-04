63f92cbd246ac7d28d56f6880de7f7bc9214b227
Mateusz Jagoda <mateusz.jagoda@itsilesia.com>
2019-07-04 14:26:14 +0200

    Add @Data to User, delete boilerplate code

--- src/main/java/com/itsilesia/auth/model/User.java
+++ src/main/java/com/itsilesia/auth/model/User.java
@@ -2,17 +2,19 @@ package com.itsilesia.auth.model;
 
 
 import com.itsilesia.auth.dto.UserDto;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Data
 @Entity
 @Table(name = "Users")
 public class User {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     private long id;
     @Column(name = "USERNAME")
@@ -24,50 +26,10 @@ public class User {
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "User_ROLES",
-            joinColumns =  @JoinColumn(name ="USER_ID"),inverseJoinColumns= @JoinColumn(name="ROLE_ID"))
+            joinColumns = @JoinColumn(name = "USER_ID"), inverseJoinColumns = @JoinColumn(name = "ROLE_ID"))
     private Set<Role> roles;
 
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public UserDto toUserDto(){
+    public UserDto toUserDto() {
         UserDto userDto = new UserDto();
         userDto.setId(this.id);
         userDto.setEmail(this.email);
